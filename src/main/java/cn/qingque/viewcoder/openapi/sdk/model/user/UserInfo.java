e54ff165d448eb4ff545e671e2584a985ead2942
tahir <kurexi@kuaishou.com>
2023-12-19 21:42:35 +0800

    Publish to maven central repository

--- src/main/java/cn/qingque/viewcoder/openapi/sdk/model/user/UserInfo.java
+++ src/main/java/cn/qingque/viewcoder/openapi/sdk/model/user/UserInfo.java
@@ -1,12 +1,14 @@
 package cn.qingque.viewcoder.openapi.sdk.model.user;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author yourname <shanrongyang@kuaishou.com>
  * Created on 2023-12-04
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class UserInfo extends UserMeta {
     private String password;
 }
