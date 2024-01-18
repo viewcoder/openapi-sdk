366de3ddad1c1d55cc158108d2b1393f8040d9f7
tahir <kurexi@kuaishou.com>
2024-01-18 16:59:40 +0800

    Refactor the Feign annotations (Support lower version of OkHttp3)

--- src/main/java/cn/qingque/viewcoder/openapi/sdk/api/InterviewClient.java
+++ src/main/java/cn/qingque/viewcoder/openapi/sdk/api/InterviewClient.java
@@ -21,7 +21,6 @@ import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
-@Headers({"Content-Type: application/json"})
 public interface InterviewClient {
     /**
      * 上传简历
@@ -34,41 +33,48 @@ public interface InterviewClient {
      * 创建面试
      */
     @RequestLine("POST /interview/create")
+    @Headers({"Content-Type: application/json"})
     Result<InterviewCreateResult> createInterview(InterviewCreateParam param);
 
     /**
      * 编辑面试
      */
     @RequestLine("POST /interview/update")
+    @Headers({"Content-Type: application/json"})
     Result<InterviewUpdateResult> updateInterview(InterviewUpdateParam param);
 
     /**
      * 获取面试列表
      */
     @RequestLine("POST /interview/list")
+    @Headers({"Content-Type: application/json"})
     Result<Page<InterviewQueryResult>> getInterviewList(InterviewQueryParam param);
 
     /**
      * 获取面试详情
      */
     @RequestLine("GET /interview/detail?interviewId={interviewId}")
+    @Headers({"Content-Type: application/json"})
     Result<InterviewDetailInfo> getInterviewDetailInfo(@Param("interviewId") Long interviewId);
 
     /**
      * 获取面试结果
      */
     @RequestLine("GET /interview/result?interviewId={interviewId}")
+    @Headers({"Content-Type: application/json"})
     Result<InterviewResult> getInterviewResult(@Param("interviewId") Long interviewId);
 
     /**
      * 提交面试结果
      */
     @RequestLine("POST /interview/result/submit")
+    @Headers({"Content-Type: application/json"})
     Result<Boolean> submitInterviewResult(InterviewResultParam param);
 
     /**
      * 获取面试结果
      */
     @RequestLine("GET /interview/template?interviewId={interviewId}")
+    @Headers({"Content-Type: application/json"})
     Result<List<InterviewTemplate>> getInterviewTemplate(@Param("interviewId") Long interviewId);
 }
